c495713ec90f103e42488347e7f0fb4541a03938
Pawel Urbanik <pawelurbanik94@gmail.com>
2018-11-19 19:33:37 +0100

    Web scopes

--- src/main/java/com/clockworkjava/kursspring/controllers/KnightController.java
+++ src/main/java/com/clockworkjava/kursspring/controllers/KnightController.java
@@ -1,6 +1,7 @@
 package com.clockworkjava.kursspring.controllers;
 
 import com.clockworkjava.kursspring.domain.Knight;
+import com.clockworkjava.kursspring.domain.PlayerInformation;
 import com.clockworkjava.kursspring.domain.repository.KnightRepository;
 import com.clockworkjava.kursspring.services.KnightService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +19,15 @@ public class KnightController {
     @Autowired
     KnightService knightService;
 
+    @Autowired
+    PlayerInformation playerInformation;
+
     @RequestMapping("/knights")
     public String getAllKnights(Model model)
     {
         List<Knight> knights= knightService.getAllKnights();
         model.addAttribute("knights",knights);
+        model.addAttribute("playerInformation", playerInformation);
         return "knights";
     }
 
@@ -30,6 +35,7 @@ public class KnightController {
     public String createKnight(Model model)
     {
         model.addAttribute("knight", new Knight());
+        model.addAttribute("playerInformation", playerInformation);
         return "knightform";
     }
 
@@ -44,6 +50,7 @@ public class KnightController {
     public String getKnight(@RequestParam("id") Integer id, Model model){
         Knight knight = knightService.getKnight(id);
         model.addAttribute("knight",knight);
+        model.addAttribute("playerInformation", playerInformation);
         return "knight";
     }
 
